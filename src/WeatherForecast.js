8058cd49bef25ac69382ac5cad6772ad1aef51a2
paulenha <paulenha@gmail.com>
2022-05-01 17:27:14 -0700

    fixed search

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./WeatherForecast.css";
 import Axios from "axios";
@@ -7,6 +7,10 @@ import WeatherForecastDay from "./ForecastDayDay";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
